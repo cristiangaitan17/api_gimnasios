df4e384fda744644dcd849258cac24700bcfc44e
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-30 15:34:27 -0500

    se creo el primer modelo de las sedes de los gimnasios

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/cristiangaitan17/api_gimnasios/config"
+	"github.com/cristiangaitan17/api_gimnasios/routes"
+)
+
+func main() {
+	config.InitDB()
+
+	router := gin.Default()
+	
+	routes.SedeRoutes(router)
+
+	log.Println("🚀 Servidor API Gimnasios corriendo en http://localhost:8080")
+	log.Fatal(router.Run(":8080"))
+}
